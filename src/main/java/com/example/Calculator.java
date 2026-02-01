e1d7c55c69e61584c6df61c5a3f70b704efb32bb
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2026-02-01 09:48:10 +0530

    Add SonarCloud CI pipeline

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -5,22 +5,22 @@ public class Calculator {
     // Code Smell: Long method + high complexity
     public int calculate(int a, int b, String op) {
 
-        if(op.equals("add")) {
+        if (op.equals("add")) {
             return a + b;
         }
-        if(op.equals("sub")) {
+        if (op.equals("sub")) {
             return a - b;
         }
-        if(op.equals("mul")) {
+        if (op.equals("mul")) {
             return a * b;
         }
-        if(op.equals("div")) {
-            if(b == 0) {
+        if (op.equals("div")) {
+            if (b == 0) {
                 return 0;
             }
             return a / b;
         }
-        if(op.equals("mod")) {
+        if (op.equals("mod")) {
             return a % b;
         }
 
